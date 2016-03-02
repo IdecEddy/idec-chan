bb708ebd6b0c6257d36821c53903a27c3e7653e0
ideceddy <edwingmundo2@gmail.com>
2016-03-02 09:31:31 -0500

    Changes to be committed: 	new file:   includes/database.php 	new file:   includes/db_connection.php 	renamed:    functions.php -> includes/functions.php 	renamed:    init.php -> includes/init.php 	new file:   includes/photograph.php 	new file:   includes/redirect.php 	new file:   includes/session.php 	new file:   includes/user.php

---
+++ includes/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+// You will need to replace thees items with the info for your database
+    defined ('DB_HOST') ? null : define('DB_HOST', "localhost");
+    defined ('DB_USER') ? null : define('DB_USER', "******");
+    defined ('DB_PASS') ? null : define('DB_PASS', "******");
+    defined ('DB_NAME') ? null : define('DB_NAME', "******");
+    
+?>
